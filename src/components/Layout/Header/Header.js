3a6d5cfd53e76dd6fbef7f982a51f7d526d30736
ZanarkandAbes <israelgrecia2001@hotmail.com>
2021-12-07 16:49:04 -0300

    exact-school-frontend: ajustando o texto do Header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -21,7 +21,7 @@ const Header = props => {
         </div>
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '0 8px' }}>
           {/* <NavLink to='/#profile_exact_school'><Image image={profileImagePath} className="div-logo-svg-container" /></NavLink> */}
-          <h1 className="h1-title-container">Perfil</h1>
+          {/* <h1 className="h1-title-container">Perfil</h1> */}
           <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '0 8px' }}>
             <button className="button-header" onClick={authContext.logout}>Sair</button>
           </div>
